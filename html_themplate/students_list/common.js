1c2e249a8da914a62ad338fc0b757993329e5550
denwayde <denwayde09@gmail.com>
2023-05-29 21:55:14 +0500

    DWDWDW

---
+++ html_themplate/students_list/common.js
@@ -0,0 +1,23 @@
+let tableBody = document.querySelector("#students-list .students-tbody")
+let stdTableRow = document.querySelector("#students-list .students-tbody tr")
+
+let dinamicStdTableRow = function(a){
+    return `
+            <tr>
+            <th scope="row">${a}</th>
+            <td>Иванов Иван Иванович</td>
+            <td>+7963254789${Math.round(Math.random()*10)}</td>
+            <td>mail@mail.ru</td>
+            <td>
+                <button type="button" class="btn btn-outline-danger">Удалить</button>
+                <button type="button" class="btn btn-outline-primary">Редактировать</button>
+                <button type="button" class="btn btn-outline-success">Выбрать</button>
+            </td>
+            </tr>
+        `
+}
+
+
+for(let i = 1; i<=20; i++){
+    tableBody.insertAdjacentHTML('beforeend', dinamicStdTableRow(i))
+}
